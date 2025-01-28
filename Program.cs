ab3966894fbe2f0ed90abeebdf33a60179ddca1a
katekk2003 <katejeffries200@gmail.com>
2025-01-28 12:06:38 -0700

    Our changes

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@
 // Check for a winner by calling the method in the supporting class, and notify the players
 // when a win has occurred and which player won the game
 
+
+using TicTacToe;
+
+Methods mt = new Methods();
 // Initialize Variables 
 string[,] gameBoard = new string[3, 3];
 bool player = true;
@@ -14,66 +18,123 @@ string player2 = 'O';
 int row = 0;
 int column = 0;
 bool isValid = false;
+bool gameOver = false;
 
 // Welcome the user to the game
 Console.WriteLine("Welcome to Tic Tac Toe!");
 
 
-// Selects a player and gets input from the player 
-if (player)
+do
 {
-    while (!isValid)
+    // Selects a player and gets input from the player 
+    if (player)
     {
-        Console.WriteLine("Player 1, you are X. Enter which row you would like to select (1-3): ");
-        row = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Enter which column you would like to select (1-3): ");
-        column = Convert.ToInt32(Console.ReadLine());
-
-        gameBoard[(row - 1), (column - 1)] = player1;
-
-        // Validates if the space is already taken
-        if (!string.IsNullOrEmpty(gameBoard[(row - 1), (column - 1)]))
+        while (!isValid)
         {
-            Console.WriteLine("That space is already taken. Please select another space.");
+            Console.WriteLine("Player 1, you are X. Enter which row you would like to select (1-3): ");
+            row = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter which column you would like to select (1-3): ");
+            column = Convert.ToInt32(Console.ReadLine());
+
+            // Validates if the space is already taken
+            if (!string.IsNullOrEmpty(gameBoard[(row - 1), (column - 1)]))
+            {
+                Console.WriteLine("That space is already taken. Please select another space.");
+            }
+            else
+            {
+                gameBoard[(row - 1), (column - 1)] = player1;
+                if (mt.checkWinner(gameBoard) != 0)
+                {
+                    Console.WriteLine($"Player {mt.checkWinner} Wins!!!");
+                    gameOver = true;
+                }
+                else if (mt.checkWinner(gameBoard) == 0)
+                {
+                    gameOver = true;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (gameBoard[i, j] == null)
+                            {
+                                gameOver = false;
+                            }
+                        }
+                    }
+                    if (gameOver)
+                    {
+                        Console.WriteLine("It's a Tie!!");
+                    }
+
+                    isValid = true;
+                }
+
+               mt.printBoard(gameBoard);
+
+
+                player = !player;
+            }
         }
-        else
+    }
+    //Same logic as the player 1 input
+    else if (!player)
+    {
+        while (!isValid)
         {
-            isValid = true;
+            Console.WriteLine("Player 2, you are O. Enter which row you would like to select (1-3): ");
+            row = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter which column you would like to select (1-3): ");
+            column = Convert.ToInt32(Console.ReadLine());
+
+            gameBoard[(row - 1), (column - 1)] = player1;
+
+            // Validates if the space is already taken
+            if (!string.IsNullOrEmpty(gameBoard[(row - 1), (column - 1)]))
+            {
+                Console.WriteLine("That space is already taken. Please select another space.");
+            }
+            else
+            {
+                gameBoard[(row - 1), (column - 1)] = player2;
+                if (mt.checkWinner(gameBoard) != 0)
+                {
+                    Console.WriteLine($"Player {mt.checkWinner} Wins!!!");
+                    gameOver = true;
+                }
+                else if (mt.checkWinner(gameBoard) == 0)
+                {
+                    gameOver = true;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (gameBoard[i, j] == null)
+                            {
+                                gameOver = false;
+                            }
+                        }
+                    }
+                    if (gameOver)
+                    {
+                        Console.WriteLine("It's a Tie!!");
+                    }
+
+                    isValid = true;
+                }
+
+                mt.printBoard(gameBoard);
+
+
+                player = !player;
+            }
         }
-
-        Console.WriteLine(gameBoard);
-
-        player = !player;
     }
 }
-//Same logic as the player 1 input
-else if (!player)
-{
-    while (!isValid)
-    {
-        Console.WriteLine("Player 2, you are O. Enter which row you would like to select (1-3): ");
-        row = Convert.ToInt32(Console.ReadLine());
+while (!gameOver);
 
-        Console.WriteLine("Enter which column you would like to select (1-3): ");
-        column = Convert.ToInt32(Console.ReadLine());
-
-        gameBoard[(row - 1), (column - 1)] = player1;
-
-        if (!string.IsNullOrEmpty(gameBoard[(row - 1), (column - 1)]))
-        {
-            Console.WriteLine("That space is already taken. Please select another space.");
-        }
-        else
-        {
-            isValid = true;
-        }
-
-        Console.WriteLine(gameBoard);
-
-        player = !player;
-    }
-}
 
 
 
