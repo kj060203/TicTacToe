a021d97f19f9396b2297d829ed1d58b33d5a0e85
katekk2003 <katejeffries200@gmail.com>
2025-01-28 11:01:01 -0700

    just instructions

--- Methods.cs
+++ Methods.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+
+//The supporting class (with whichever name you choose) will:
+// Receive the “board” array from the driver class
+// Contain a method that prints the board based on the information passed to it
+// Contain a method that receives the game board array as input and returns if there is a
+// winner and who it was
 
 namespace TicTacToe
 {
--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿// The “Driver” class (the Program.cs class with the main method where the program begins) will:
+// Welcome the user to the game
+//  Create a game board array to store the players’ choices
+// Ask each player in turn for their choice and update the game board array
+// Print the board by calling the method in the supporting class
+// Check for a winner by calling the method in the supporting class, and notify the players
+// when a win has occurred and which player won the game
